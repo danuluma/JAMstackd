d85bec5c2cf1b8c92a2e6c337adf9a371c840e4c
Dan Uluma <danuluma@gmail.com>
2018-08-31 17:31:22 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>My country people</h1>
+    <p>How y'all doing</p>
+    <p>Welcome to kenya</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Hello from the other side</h1>
+    <p>Rudi ulikotoka!</p>
+    <Link to="/">Welcome!</Link>
+  </div>
+)
+
+export default SecondPage
